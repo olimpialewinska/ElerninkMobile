2e23923597a40d8d4d34adb5c2ef005199696f32
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-03-31 16:33:04 +0200

    initial commit

---
+++ components/Regitster/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
+
+export function Register() {
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
